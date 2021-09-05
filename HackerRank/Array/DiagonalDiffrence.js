8e76c8f2cab7775fda4e566f52c7b7b06e38a41a
Rushil dhinoja <mr.rushil17@gmail.com>
2021-09-05 23:44:05 +0530

    Added Explanation for both the problems from hackerrank

--- HackerRank/Array/DiagonalDiffrence.js
+++ HackerRank/Array/DiagonalDiffrence.js
@@ -16,6 +16,16 @@
 //Output 15
 
 //Solution Starts
+
+//Explanation
+//Here first of all we have to understand that given that it is a square matrix the length of the inputData will be equal to the length of array on every index that means if length of inputData = 3 then length of inputData[0] = 3;
+//From this we can loop through the inputArray until the length is less than length of inputArray
+//Now, first of all we will declare 4 variables two for holding the sum of both the diagonals and two for holding the currentIndex of both the diagonals;
+//Index of first diagonal will be 0
+//Index of second diagonal will be inputArray.length - 1;
+//Then, we will loop through all the elements of inputArray while adding the sum of inputArray[index][i] to sum of first diagonal and inputArray[index][j] to second diagonal;
+//when the loop ends we will return the absolute difference of both the diagonals
+
 const inputData = [
   [1, 2, 3],
   [4, 5, 6],
@@ -23,21 +33,31 @@ const inputData = [
 ];
 
 function getDiagonalDifference(inputArray) {
+  //Variable to hold sum of fist diagonal (Left to Right)
   let firstDiagonalSum = 0;
+  //Variable to hold sum of fist diagonal (Right to Left)
   let secondDiagonalSum = 0;
+  //Variable to keep hold of index of first diagonal
   let i = 0;
+  //Variable to keep hold of index of second diagonal
   let j = inputArray.length - 1;
 
+  //Loop thorough the input array.
   for (let index = 0; index < inputArray.length; index++) {
+    //Add sum to first diagonal
     firstDiagonalSum += inputArray[index][i];
+    //Add sum to second diagonal
     secondDiagonalSum += inputArray[index][j];
+    //Increase the index of first diagonal
     i++;
+    //Decrease the index of first diagonal
     j--;
   }
-
+  //Return absolute difference
   return Math.abs(firstDiagonalSum - secondDiagonalSum);
 }
 
 console.log(getDiagonalDifference(inputData));
 
+//Time Complexity: O(n)
 //Solution Ends
--- HackerRank/Array/StairCase.js
+++ HackerRank/Array/StairCase.js
@@ -9,17 +9,27 @@
 // ####
 
 //Solution Starts
+//Explanation
+//Note: In this solution some methods are used which are JavaScript Specific
+//In this we will first of declare an empty array of size same as N;
+//After that we will loop until i = 0 and in the beginning i will be n-1;
+//We will replace the i (th) element with the '#' symbol and use the join() method to console log a string;
 
 const inputData = 4;
 
 function stairCase(n) {
-  const newArray = Array(n).fill(' ');
+  //Create an empty array of size n and fill it with blank space
+  const newArray = Array(n).fill(" ");
+  //Loop until i = 0;
   for (let i = n - 1; i >= 0; i--) {
-    newArray[i] = '#';
-    console.log(newArray.join(''));
+    //Replace the i (th) element of array with '#'
+    newArray[i] = "#";
+    //Log the string array using join() function
+    console.log(newArray.join(""));
   }
 }
 
 stairCase(inputData);
 
+//Time Complexity: O(n)
 //Solution Ends
